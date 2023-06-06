8851544d7968dfdbb1a8a1039fb0592153e2697c
Mohammed Fananah <Hp@HP>
2023-06-06 11:07:06 +0300

    Add project files.

---
+++ BankTransactions/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BankTransactions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
